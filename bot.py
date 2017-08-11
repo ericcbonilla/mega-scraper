b199d0fa4c5a771c6e6de65f0605a79c455518a1
ericcbonilla <ericcbonilla@gmail.com>
2017-08-11 00:07:06 -0700

    Switched over to python3's venv, added a requirements.txt, and a couple minor changes

--- bot.py
+++ bot.py
@@ -3,12 +3,10 @@
 # HvC9eLJz7DbZuc-XZcxTl8WKXxU
 # BlL4ObqPdeCFWg
 
-# TODO: reinstall pip, python
-# TODO: posts need to be yielded through Subreddit.submissions with start_date = time of last query and end_date = current time
-# time of last query must be stored globally
+# TODO: posts need to be yielded through Subreddit.submissions with start_date = time of last query and end_date = current time. Time of last query must be stored globally
 
 import praw
-import unicodedata
+import unicodedata # For some reason the post attributes are now fetched as type str and not type unicode after switching virtual environments, so we don't need this anymore
 from constants import *
 
 def truncate(text, max_length=50):
@@ -17,21 +15,13 @@ def truncate(text, max_length=50):
     else:
         return text
 
-# print (BOT)
 reddit = praw.Reddit(SITE_NAME, user_agent=USER_AGENT)
 for sub in SUBREDDIT_LIST:
-    # for post in reddit.subreddit(sub).new(limit=1):
     for post in reddit.subreddit(sub).submissions(1502323200, 1502423555):
-
-        selftext = unicodedata.normalize('NFKD', post.selftext).encode('ascii','ignore')
-        title = unicodedata.normalize('NFKD', post.title).encode('ascii','ignore')
-        url = unicodedata.normalize('NFKD', post.url).encode('ascii','ignore')
-
-        if (QUERY in title):
-            print(post)
+        if (QUERY in post.title):
             print(sub)
-            print("Title: ", title)
-            print("URL: ", url)
+            print("Title: ", post.title)
+            print("URL: ", post.url)
             # print("Text: ", truncate(selftext))
             # print("Score: ", post.score)
             print("---------------------------------\n")
