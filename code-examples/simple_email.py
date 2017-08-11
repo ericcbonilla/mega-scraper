7eb4dc8319fcefbb642b34b051695f8f7f0e1289
Manny1995 <immanuelda@gmail.com>
2017-08-11 00:45:20 -0700

    finished email example

--- code-examples/simple_email.py
+++ code-examples/simple_email.py
@@ -1,23 +1,10 @@
-# Import smtplib for the actual sending function
+# Created by Immanuel Amirtharaj
+
 import smtplib
 
-# Import the email modules we'll need
-from email.mime.text import MIMEText
 
-# Open a plain text file for reading.  For this example, assume that
-# the text file contains only ASCII characters.
-textfile = "file.txt"
-with open(textfile) as fp:
-    # Create a text/plain message
-    msg = MIMEText(fp.read())
-
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = 'The contents of %s' % textfile
-msg['From'] = "immanuelda@gmail.com"
-msg['To'] = "immanuelda@gmail.com"
-
-# Send the message via our own SMTP server.
-s = smtplib.SMTP('localhost', 8025)
-s.send_message(msg)
-s.quit()
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login('megascraper2017@gmail.com', 'goonsquad')
+smtpObj.sendmail('megascraper2017@gmail.com', 'ericcbonilla@gmail.com', 'Subject: IT WORKS BROTHER')
