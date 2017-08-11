0da5cb629e0522c397f97870eac39c5e8955dfae
ericcbonilla <ericcbonilla@gmail.com>
2017-08-10 22:00:44 -0700

    Updated submission fetching to allow a query

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@
 # BlL4ObqPdeCFWg
 
 # TODO: reinstall pip, python
+# TODO: posts need to be yielded through Subreddit.submissions with start_date = time of last query and end_date = current time
+# time of last query must be stored globally
 
 import praw
 import unicodedata
@@ -20,9 +22,15 @@ reddit = praw.Reddit(SITE_NAME, user_agent=USER_AGENT)
 for sub in SUBREDDIT_LIST:
     # for post in reddit.subreddit(sub).new(limit=1):
     for post in reddit.subreddit(sub).submissions(1502323200, 1502423555):
+
         selftext = unicodedata.normalize('NFKD', post.selftext).encode('ascii','ignore')
-        print(sub)
-        print("Title: ", post.title)
-        # print("Text: ", truncate(selftext))
-        # print("Score: ", post.score)
-        print("---------------------------------\n")
+        title = unicodedata.normalize('NFKD', post.title).encode('ascii','ignore')
+        url = unicodedata.normalize('NFKD', post.url).encode('ascii','ignore')
+
+        if (QUERY in title):
+            print(sub)
+            print("Title: ", title)
+            print("URL: ", url)
+            # print("Text: ", truncate(selftext))
+            # print("Score: ", post.score)
+            print("---------------------------------\n")
--- constants.py
+++ constants.py
@@ -3,3 +3,4 @@ SITE_NAME = 'megabot'
 USER_AGENT = 'megabot agent 0.1'
 SUBREDDIT_LIST = ['megalinks', 'mega']
 MULTIREDDIT = [sub for sub in SUBREDDIT_LIST] #concat with +'s into a single string'
+QUERY = 'MOVIE'
