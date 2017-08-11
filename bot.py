af872f234c8a88cfd7e547f40e92a093c85ccb35
ericcbonilla <ericcbonilla@gmail.com>
2017-08-10 20:54:24 -0700

    Pulling some submissions and stuff...

--- bot.py
+++ bot.py
@@ -2,3 +2,27 @@
 # password123
 # HvC9eLJz7DbZuc-XZcxTl8WKXxU
 # BlL4ObqPdeCFWg
+
+# TODO: reinstall pip, python
+
+import praw
+import unicodedata
+from constants import *
+
+def truncate(text, max_length=50):
+    if len(text) > max_length:
+        return text[:max_length] + '...'
+    else:
+        return text
+
+# print (BOT)
+reddit = praw.Reddit(SITE_NAME, user_agent=USER_AGENT)
+for sub in SUBREDDIT_LIST:
+    # for post in reddit.subreddit(sub).new(limit=1):
+    for post in reddit.subreddit(sub).submissions(1502323200, 1502423555):
+        selftext = unicodedata.normalize('NFKD', post.selftext).encode('ascii','ignore')
+        print(sub)
+        print("Title: ", post.title)
+        # print("Text: ", truncate(selftext))
+        # print("Score: ", post.score)
+        print("---------------------------------\n")
---
+++ constants.py
@@ -0,0 +1,5 @@
+BOT = 'bot'
+SITE_NAME = 'megabot'
+USER_AGENT = 'megabot agent 0.1'
+SUBREDDIT_LIST = ['megalinks', 'mega']
+MULTIREDDIT = [sub for sub in SUBREDDIT_LIST] #concat with +'s into a single string'
