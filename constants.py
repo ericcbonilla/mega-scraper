4764de31cf80184de04df41b30fad85a7d73bc91
ericcbonilla <ericcbonilla@gmail.com>
2017-08-19 11:49:07 -0700

    Starting to render forms

--- constants.py
+++
@@ -1,6 +0,0 @@
-BOT = 'bot'
-SITE_NAME = 'megabot'
-USER_AGENT = 'megabot agent 0.1'
-SUBREDDIT_LIST = ['megalinks', 'mega']
-MULTIREDDIT = [sub for sub in SUBREDDIT_LIST] #TODO: oncat with +'s into a single string'
-QUERY = 'MOVIE'
--- src/flask/app.py
+++ src/flask/app.py
@@ -1,22 +1,20 @@
 # Created by Immanuel Amirtharaj
 # app.py
 
-from flask import Flask, request
-# from flask_sqlalchemy import SQLAlchemy
 import peewee
-from models import Task, User, Subscriber
-
-from apscheduler.schedulers.background import BackgroundScheduler
 import time
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, request
+from models import Task, User, Subscriber
+from forms import AddTaskForm
 
 app = Flask(__name__)
-
 
 # export FLASK_APP=app.py
 # flask run
 
 def foo():
-	print("I am a job")
+    print("I am a job")
 
 scheduler = BackgroundScheduler()
 scheduler.start()
@@ -24,59 +22,71 @@ scheduler.add_job(foo, 'interval', seconds=5)
 
 
 def create_or_find_user(email):
-	try:
-		found_user = User.get(User.email == email)
-	except User.DoesNotExist:
-		User.create(email=email).save()
-		found_user = User.get(User.email == email)
+    try:
+        found_user = User.get(User.email == email)
+    except User.DoesNotExist:
+        User.create(email=email).save()
+        found_user = User.get(User.email == email)
 
-	return found_user
+    return found_user
 
 
 def create_or_find_task(keywords):
-	try:
-		found_task = Task.get(Task.keywords == keywords)
-	except Task.DoesNotExist:
-		Task.create(keywords=keywords).save()
-		found_task = Task.get(Task.keywords == keywords)
+    try:
+        found_task = Task.get(Task.keywords == keywords)
+    except Task.DoesNotExist:
+        Task.create(keywords=keywords).save()
+        found_task = Task.get(Task.keywords == keywords)
 
-	return found_task
+    return found_task
 
 
 def create_or_find_subscriber(user_id, task_id):
-	try:
-		found_subscriber = Subscriber.get(Subscriber.userId == user_id, Subscriber.taskId == task_id)
-	except Subscriber.DoesNotExist:
-		Subscriber.create(userId=user_id, taskId = task_id).save()
-		found_subscriber = Subscriber.get(Subscriber.userId == user_id, Subscriber.taskId == task_id)
+    try:
+        found_subscriber = Subscriber.get(Subscriber.userId == user_id, Subscriber.taskId == task_id)
+    except Subscriber.DoesNotExist:
+        Subscriber.create(userId=user_id, taskId = task_id).save()
+        found_subscriber = Subscriber.get(Subscriber.userId == user_id, Subscriber.taskId == task_id)
 
-	return found_subscriber
-
+    return found_subscriber
 
 
 # Handle email
 @app.route('/tasks/new', methods=['POST'])
 def addTask():
 
-	email = request.form['email']
-	keywords = request.form['keywords']
+    email = request.form['email']
+    keywords = request.form['keywords']
 
-	new_task = create_or_find_task(keywords)
-	new_user = create_or_find_user(email)
+    new_task = create_or_find_task(keywords)
+    new_user = create_or_find_user(email)
 
-	create_or_find_subscriber(new_user.id, new_task.id)
+    create_or_find_subscriber(new_user.id, new_task.id)
 
-	return "Success"
+    return "Success"
 
+
+@app.route('/forms/add-task', methods=['GET', 'POST'])
+def addTaskThroughForm():
+
+    form = AddTaskForm(request.form)
+    if request.method == 'POST':
+        new_task = create_or_find_task(form.query.data)
+        new_user = create_or_find_user(form.email.data)
+        subscriber = create_or_find_subscriber(new_user.id, new_task.id)
+
+        flash('New subscriber created: ' + str(subscriber.id))
+        # return redirect(url_for('login'))
+    return render_template('templates/add_task.html', form=form)
 
 
 @app.route('/')
 def index():
-	print("Entered the index")
-	return "megalink scraper"
+    print("Entered the index")
+    return "megalink scraper index"
 
 # For heroku, when we deploy
 if __name__ == '__main__':
-	# Bind to PORT if defined, otherwise default to 5000.
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
